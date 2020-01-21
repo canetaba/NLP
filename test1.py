42dd92359a517bd7bbee954e1455324f848d4f93
Maria Jose Briceno Nikulin <maria.briceno@sonda.com>
2020-01-21 18:57:12 -0300

    archivos tarea

---
+++ test1.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import regex as re
+import nltk
+import numpy as np
+import logging # verbosidad (útil para saber qué está ocurriendo al ejecutar código)
+
+
+nltk.download('stopwords')
+nltk.download('punkt')
+stopwords = nltk.corpus.stopwords.words('spanish')
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+def cargar_datos_df(archivo):
+    # Carga los archivos en un dataframe
+    return pd.read_csv(archivo, sep="\t", header=None)
+
+def cargar_datos(archivo):
+    # Carga los archivos en un arreglo
+    oraciones = []
+    with open(ruta, encoding='utf-8', errors='ignore') as file:  # Usamos utf-8 para preservar los caracteres especiales
+        for line in file:
+            oraciones.append((line.rstrip()))  # Usamos la función strip para remover el caracter \n (nueva línea)
+    return oraciones
+
+
+def limpiar_datos(text):
+    text = text.lower()  # Todo a minúsculas
+    text = re.sub(r'[^A-Za-zñáéíóú]', ' ', text)  # Reemplaza todas los signos de puntuación por un espacio
+    text = re.sub('á', 'a', text)  # Reemplaza las vocales con tilde por su forma basal
+    text = re.sub('é', 'e', text)
+    text = re.sub('í', 'i', text)
+    text = re.sub('ó', 'o', text)
+    text = re.sub('ú', 'u', text)
+    return text
+
+
+def vectorizer(text, model):
+    # Vectorizamos
+    vectors = []
+    for i in text:
+        try:
+            vectors.append(model.wv[i])
+        except:
+            pass
+    return(np.mean(vectors,axis=0))
+
+
+ruta = "archivo_chico.txt"
+sentences = cargar_datos(ruta)
+print(sentences)
+
+# Creamos un diccionario con la lista de conversaciones
+conversaciones = { i : sentences[i] for i in range(0, len(sentences) ) }
+
+
+preguntas = []
+respuestas = []
+for i in range(0, len(conversaciones)):
+    m = i
+    if m%2:
+        respuestas.append(conversaciones.get(i))
+    else:
+        preguntas.append(conversaciones.get(i))
+
+print ("Preguntas: ", preguntas)
+print("Respuestas ", respuestas)
+
+
+
+# Limpiamos los datos
+# Normalizamos las preguntas y las respuestas
+preguntas_normalizadas = [limpiar_datos(pregunta) for pregunta in preguntas]
+#print(preguntas_normalizadas)
+
+respuestas_normalizadas = [limpiar_datos(respuesta) for respuesta in respuestas]
+#print(respuestas_normalizadas)
+
+
+# Tokenizamos
+tokenized_preguntas = [nltk.word_tokenize(pregunta) for pregunta in preguntas_normalizadas]
+tokenized_respuestas = [nltk.word_tokenize(respuesta) for respuesta in respuestas_normalizadas]
+# print(tokenized_preguntas)
+# print(tokenized_respuestas)
+
+# Removemos las stopwords
+without_stopwords_preguntas = []
+without_stopwords_respuestas = []
+
+for palabra in tokenized_preguntas:
+    without_stopwords_palabra = [word for word in palabra if word not in stopwords]
+    without_stopwords_preguntas.append(without_stopwords_palabra)
+
+for palabra in tokenized_respuestas:
+    without_stopwords_palabra = [word for word in palabra if word not in stopwords]
+    without_stopwords_respuestas.append(without_stopwords_palabra)
+
+print (without_stopwords_preguntas)
+print (without_stopwords_respuestas)
